36d068ea7fa5f9caffee03b7fd19faf8b8c2f96d
hadyelsahar <hadyelsahar@gmail.com>
2014-12-12 04:06:12 +0200

    added Logistic regression classifier
    added logistic regression classifier with L1 penalty as well to
    select best feature and compare best accuracy with the svm
    classifier

--- L1SVC.py
+++ L1SVC.py
@@ -9,89 +9,106 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer 
 from sklearn import cross_validation
 from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression 
 
 from nltk.tokenize import TreebankWordTokenizer
 
-
 datasets  = ["QYM","LABR"]
+classifiers = { 'LSVC' : LinearSVC,
+                'LREG' : LogisticRegression
+                }
+
 #Reading Datasets 
 for dataset_name in datasets:
-    print " Experimenting dataset " + dataset_name
+    for classifier_name,classifier_class in classifiers.items():
+        print " Experimenting dataset " + dataset_name
 
-    # Reading datasets
-    docs = pd.read_csv('./datasets/'+dataset_name+'.csv',
-                delimiter=",",encoding="utf-8")
+        # Reading datasets
+        docs = pd.read_csv('./datasets/'+dataset_name+'.csv',
+                    delimiter=",",encoding="utf-8")
 
-    # Splitting dataset to Train and Test
-    (trainids,testids) = cross_validation.train_test_split(docs.index,
-                            train_size=0.8,test_size = 0.2,random_state=2)
-    train = docs.loc[trainids]
-    test = docs.loc[testids]
+        # Splitting dataset to Train and Test
+        (trainids,testids) = cross_validation.train_test_split(
+                                docs.index,
+                                train_size=0.8,
+                                test_size = 0.2,random_state=2)
 
-    # Building TFIDF Vectors X (vector of TFIDF weights)
-    # y (polarity class of the vector)
+        train = docs.loc[trainids]
+        test = docs.loc[testids]
 
-    vectorizer = TfidfVectorizer(tokenizer=TreebankWordTokenizer().tokenize,
-            ngram_range=(1,2))
-    Xtrain = vectorizer.fit_transform(train.text)    
-    ytrain = train.polarity
-    Xtest = vectorizer.transform(test.text)
-    ytest= test.polarity
+        # Building TFIDF Vectors X (vector of TFIDF weights)
+        # y (polarity class of the vector)
+        vectorizer = TfidfVectorizer(
+                        tokenizer=TreebankWordTokenizer().tokenize,
+                        ngram_range=(1,3))
 
-    fn = vectorizer.get_feature_names() #feature names
-    acc = []                            #accuracy
-    fc = []                             #feature counts for each iteration
-    fs = []                  #feature ids for each iteration
+        Xtrain = vectorizer.fit_transform(train.text)    
+        ytrain = train.polarity
+        Xtest = vectorizer.transform(test.text)
+        ytest= test.polarity
 
-    cr = [pow(10,i) for i in range(-2,6,1)]
-    # cr = arange(1.4,1.8,0.01)   
-    for c in cr:
-        try :
-            svc = LinearSVC(C=c, penalty="l1", dual=False)
-            Xtrain_new = svc.fit_transform(Xtrain,ytrain)
-            predicted = svc.predict(Xtest)
-            
-            accuracy = np.mean(predicted == ytest)
-            feature_count = Xtrain_new.shape[1]
+        fn = vectorizer.get_feature_names() #feature names
+        acc = []                            #accuracy
+        fc = []                             #feature counts for each iteration
+        fs = []                  #feature ids for each iteration
 
-            print "dataset : " + dataset_name + "acc = " + str(accuracy) + \
-                    " at c = " + str(c)  + " and n_features = " + \
-                    str(feature_count) + " and f_% = " + \
+        cr = [pow(10,i) for i in range(-2,6,1)]
+        # cr = arange(1.4,1.8,0.01)
+        for c in cr:
+            try :
+                svc = classifier_class(C=c, penalty="l1", dual=False)
+                Xtrain_new = svc.fit_transform(Xtrain,ytrain)
+                predicted = svc.predict(Xtest)
+                
+                accuracy = np.mean(predicted == ytest)               
+                feature_count = Xtrain_new.shape[1]
+
+                print "dataset : %s acc = %s at c = %s \
+                and feat no. = %s and f_percent = %s"
+                %(dataset_name,accuracy,
+                    c,feature_count,
                     str(float(feature_count)/Xtrain.shape[1])
+                    )
 
-            acc.append(accuracy)
-            fc.append(feature_count)
-            fs.append(dict([(i,v) for i,v in enumerate(svc.coef_[0]) if v != 0 ]))
+                acc.append(accuracy)
+                fc.append(feature_count)
 
-        except : 
-            pass
-    
+                tmp = dict([(i,v) for i,v in enumerate(svc.coef_[0]) if v != 0 ])
+                fs.append(tmp)
 
-    # logging results to files
+            except ValueError, e:
+                print e 
+                print "at dataset : %s at c = %s "\
+                        %(dataset_name,c)
+                pass
+        
 
-    prefix = "./experiments-results/"
-    f_bestfeatures = open(prefix + \
-        "L1SVC_" + dataset_name + "_bestfeatures.csv","w")
-    f_acc = open(prefix + "L1SVC_" + \
-    	dataset_name + "_nf_vs_acc.csv","w")
+        # logging results to files
 
-    f_acc.write("features_number,accuracy,feature_precentage")    
-    for i,c in enumerate(acc):
-        perc = float(fc[i])/Xtrain.shape[1]
-        f_acc.write("\n%s,%s,%s"%(fc[i],c,perc))
+        prefix = "./experiments-results/"
+        f_bestfeatures = open(prefix + \
+            classifier_name + "_" + dataset_name + "_bestfeatures.csv","w")
+        f_acc = open(prefix + classifier_name + "_" + \
+            dataset_name + "_nf_vs_acc.csv","w")
 
-    # get features for max accuracy #if max accuracy get the minimum feature counts
-    # acc is sorted according to increasing of number of features (increasing of c)
-    
-    id_maxacc = acc.index(max(acc))
-    best_features = fs[id_maxacc]
-    best_features = sorted(best_features.items(), key=operator.itemgetter(1))
-    
-    s = "\n".join([fn[i].encode("utf-8") + "," + str(w) for i,w in best_features])
-    f_bestfeatures.write("ngram,weight\n"+s)
+        f_acc.write("features_number,accuracy,feature_precentage")    
+        for i,c in enumerate(acc):
+            perc = float(fc[i])/Xtrain.shape[1]
+            f_acc.write("\n%s,%s,%s"%(fc[i],c,perc))
 
-    f_bestfeatures.close()
-    f_acc.close()
+        # get features for max accuracy 
+        #if max accuracy get the minimum feature counts
+        # acc is sorted according to increasing of number of features
+        
+        id_maxacc = acc.index(max(acc))
+        best_features = fs[id_maxacc]
+        best_features = sorted(best_features.items(), key=operator.itemgetter(1))
+        
+        s = "\n".join([fn[i].encode("utf-8") + "," + str(w) for i,w in best_features])
+        f_bestfeatures.write("ngram,weight\n"+s)
+
+        f_bestfeatures.close()
+        f_acc.close()
 
 
-    
+        
