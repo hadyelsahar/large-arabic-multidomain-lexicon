fae21cab53fdbb11304a95002e4d7154ab8425b1
hadyelsahar <hadyelsahar@gmail.com>
2015-01-03 20:53:20 +0200

    deleted qayem old folder name

--- scrappers/qayem/qayem/items.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import scrapy
-
-class Review(scrapy.Item):    
-    restaurant_id = scrapy.Field()
-    user_id = scrapy.Field()
-    text = scrapy.Field()
-    polarity = scrapy.Field()
-   
