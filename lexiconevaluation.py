443695a0624e43c1b395aa8a3b25f824dced5609
hadyelsahar <hadyelsahar@gmail.com>
2015-01-06 19:43:48 +0200

    Add loops for Different experiments  to test different classifiers with differet features
    . multiple setups for vectorizers (TF-idf, count, lexicon)
    . multiple feature unions  TF-idf, tfidf + lexicon ..etc
    . different setups for datasets (CV, balanced/unbalanced, 3C/2C

--- lexiconevaluation.py
+++ lexiconevaluation.py
@@ -14,21 +14,19 @@ from sklearn import cross_validation
 from sklearn import metrics
 from sklearn.feature_selection import VarianceThreshold
 from sklearn import preprocessing
-
 from sklearn import cross_validation
-
-
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
 from classes.Document import *
 from classes.Utils import * 
 from classes.LexiconVectorizer import * 
 
-parser = argparse.ArgumentParser(description='script for designing experiments sentiment classification upon created datasets and generated lexicons')
-parser.add_argument('-d','--dataset', help='which dataset to run experiment on',required=False)
-args = parser.parse_args()
-
-
 valid_datasets = ["LABR","SUQ","QYM","TRH","TRA","TRR","MOV"]
+
+parser = argparse.ArgumentParser(description='script for designing experiments sentiment classification upon created datasets and generated lexicons')
+parser.add_argument('-d','--dataset',
+    help='which dataset to run experiment on',required=False)
+args = parser.parse_args()
 if args.dataset is None : 
     datasets = valid_datasets
 else :
@@ -41,77 +39,123 @@ else :
 
 # c = {"TRH":100,"QYM":30}
 
-print datasets
-
 for dname in datasets : 
 
-    #Building Features 
+    vectorizers  = {
 
-    #vectorizers : 
-    #TFIDF
-    tfidf_vectorizer = TfidfVectorizer(
+                    "tfidf" : TfidfVectorizer(
                             tokenizer=TreebankWordTokenizer().tokenize,
                             ngram_range=(1,2),norm="l1",
                             preprocessor = Document.preprocess
-                            )
-
-    #Count
-    count_vectorizer = CountVectorizer()
-
-    #Lexicon Vectorizers
-    lex_vectorizer = LexiconVectorizer(lexfile='lexicon/%s_lex.csv'%dname,
-                        polarity = True,
-                        weightedcount = True,
-                        preprocessor = Document.preprocess
+                        ),
+                    "count" : CountVectorizer(
+                            tokenizer=TreebankWordTokenizer().tokenize,
+                            ngram_range=(1,2),
+                            preprocessor = Document.preprocess
+                        ),
+                    "lex-domain" : LexiconVectorizer(
+                            lexfile='lexicon/%s_lex.csv'%dname,
+                            polarity = True,
+                            weightedcount = True,
+                            preprocessor = Document.preprocess
+                        ),
+                    "lex-all" : LexiconVectorizer(
+                            lexfile='lexicon/ALL_lex.csv',
+                            polarity = True,
+                            weightedcount = True,
+                            preprocessor = Document.preprocess
                         )
+    }
 
-    lex_all_vectorizer = LexiconVectorizer(lexfile='lexicon/ALL_lex.csv',
-                        polarity = True,
-                        weightedcount = True,
-                        preprocessor = Document.preprocess
-                        )
+    kfolds = {                
+                "CV_unBalanced_2C" : create_dataset(dname, 
+                    CV = True, neutral = False, balanced = False, n_folds = 5
+                    ),
+                "CV_unBalanced_3C" : create_dataset(dname, 
+                    CV = True, neutral = True, balanced = False, n_folds = 5
+                    ),
+                "CV_Balanced_2C" : create_dataset(dname, 
+                    CV = True, neutral = False, balanced = True, n_folds = 5
+                    ),
+                "CV_Balanced_3C" : create_dataset(dname, 
+                    CV = True, neutral = True, balanced = True, n_folds = 5
+                    ),
+                "split_unBalanced_2C" : create_dataset(dname, 
+                    CV = False, neutral = False, balanced = False, n_folds = 5
+                    )
+    }
+
+
+    classifiers = {
+                "svc" : LinearSVC(penalty="l1", dual=False),
+                # "svc_penalized" : LinearSVC(penalty="l1", dual=False, C= c[dname])
+    }
 
 
     #Feature Building
-    features = FeatureUnion([
-        # ("count", count_vectorizer),
-        ("bool_lex", lex_all_vectorizer),
-        ("tfidf", tfidf_vectorizer)
-        ])
-
-    #Feature Selecting 
-    # selector = VarianceThreshold()
-
-    #Classifiers
-    # svc = LinearSVC(penalty="l1", dual=False, C= c[dname])
-    svc = LinearSVC(penalty="l1", dual=False)
-
-    #Pipeline 
-    classifier = Pipeline([
-        ('features', features), 
-        # ('select',selector), 
-        ('svc', svc)])
+    features = {
+                "lex-domain" : FeatureUnion([
+                        ("lex-domain", vectorizers["lex-domain"])]
+                        ),
+                "lex-all" : FeatureUnion([
+                        ("lex-all", vectorizers["lex-all"])]
+                        ),
+                "tfidf" : FeatureUnion([
+                        ("tfidf", vectorizers["tfidf"])]
+                        ),
+                "count" : FeatureUnion([
+                        ("count", vectorizers["count"])]
+                        ),
+                "tfidf_lex-domain" : FeatureUnion([
+                        ("lex-domain", vectorizers["lex-domain"]),
+                        ("tfidf", vectorizers["tfidf"])]
+                        ),
+                "tfidf_lex-all" : FeatureUnion([
+                        ("lex-all", vectorizers["lex-all"]),
+                        ("tfidf", vectorizers["tfidf"])]
+                        ),
+                "count_lex-domain" : FeatureUnion([
+                        ("lex-domain", vectorizers["lex-domain"]),
+                        ("count", vectorizers["count"])]
+                        ),
+                "count_lex-all" : FeatureUnion([
+                        ("lex-all", vectorizers["lex-all"]),
+                        ("count", vectorizers["count"])]
+                        )
+    }
 
     
-    #dataset prepataion 
-    fold = create_dataset(dname, CV = True, 
-                    neutral = True, balanced = True, n_folds = 5)
-    arr = []
-    for (X_train,y_train,X_test,y_test)  in fold:
+    for fvector_name,fvector in features.items():
+        for clf_name, clf in classifiers.items():
+            for fold_name,fold in kfolds.items():
 
-        classifier.fit_transform(X_train,y_train)
-        pred = classifier.predict(X_test)
+                print "# %s\t%s\t%s\t%s"%(dname, fold_name, fvector_name, clf_name)
+                pipeline = Pipeline([
+                                ('features', fvector), 
+                                # ('select',selector), 
+                                ('classifier', clf)])
 
-        a1 = np.array(precision_recall_fscore_support(y_test, pred))
-        a2 = np.array(precision_recall_fscore_support(y_test, pred, average = "micro"))
-        a2[3] = np.sum(a1[3,:]) #adding support count = sum of all supports
-        arr.append(np.c_[a1,a2])    
+                # for each of the cross folds 
+                # calculate the metrics and store them in array
+                arr = []                
+                for (X_train,y_train,X_test,y_test)  in fold:
 
+                    pipeline.fit_transform(X_train,y_train)
+                    pred = pipeline.predict(X_test)
 
-    metrics = np.array([sum([i[x] for i in arr])/len(arr) for x in range(4)])
-    x = pd.DataFrame(metrics).transpose()
-    x.index = ["neg","neutral","pos","Average/Total"] if len(metrics[0]) == 4 \
-                else ["neg","pos","Average/Total"]
-    x.columns = ["precision","recall","fscore","support"]
-    print x 
+                    a1 = np.array(precision_recall_fscore_support(y_test, pred))
+                    a2 = np.array(precision_recall_fscore_support(y_test, pred, 
+                        average = "micro"))
+                    a2[3] = np.sum(a1[3,:]) #support count = sum of all supports
+                    arr.append(np.c_[a1,a2])    
 
+                # average all the metrics of all cross folds
+                metrics = np.mean(np.array(arr),axis = 0)                
+                x = pd.DataFrame(metrics).transpose()
+
+                ic3 = ["neg","neutral","pos","Average/Total"]
+                ic2 = ["neg","pos","Average/Total"]
+                x.index =  ic3 if len(metrics[0]) == 4 else ic2                 
+                x.columns = ["precision","recall","fscore","support"]
+
+                print x 
